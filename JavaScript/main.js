c42b1c88609381f7e07e4b81c093598a1848a977
danders <diegoganders@gmail.com>
2023-01-04 15:41:23 -0300

    operadores avanzados + corrección de css

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -36,11 +36,15 @@ formulario.addEventListener("submit", (e) => {
     const edad = document.getElementById("edad");
     const genero = document.getElementById("genero");
     contador = 0;
+    //Creo un objeto y lo cargo con lo que me traigo del form
     Participante1 = new Participante(nombre.value, edad.value, genero.value);
     formulario.reset();
-    console.log("Su nombre es: " + Participante1.nombre);
-    console.log("Su edad es: " + Participante1.edad);
-    console.log("Su genero es: " + Participante1.genero);
+    //muestro los datos cargados por consola con operador ternario
+    (Participante1.nombre != "") ? console.log("Su nombre es: " + Participante1?.nombre) : console.log("No se cargó nombre");
+    (Participante1.edad != "") ? console.log("Su edad es: " + Participante1?.edad) : console.log("No se cargó edad");
+    (Participante1.genero != "") ? console.log("Su genero es: " + Participante1?.genero) : console.log("No se cargó género");
+
+    //Genero el enunciado, la primera imagen y los botones para jugar
     document.getElementById("enunciado").innerText = "      A continución usted deberá seleccionar si la imagen se corresponde a un intento de phshing!"
     document.getElementById("participante").innerHTML = "Participante: " + Participante1.nombre;
     console.log(juego1);
@@ -53,6 +57,9 @@ formulario.addEventListener("submit", (e) => {
     imagenes.forEach(function (elemento) {
         console.log(elemento);
     })
+
+
+    /*
     const contenedorBotones = document.getElementById("botonesParticipante");
     const card = document.createElement("div");
     card.innerHTML = `
@@ -60,10 +67,16 @@ formulario.addEventListener("submit", (e) => {
     <br><br>
     `
     contenedorBotones.appendChild(card);
-
+*/
     //Guardo al participante en el localStorage
     localStorage.setItem("participante", JSON.stringify(Participante1));
 
+    //Pruebo operador OR con Falsy
+    const participantestorage = JSON.parse(localStorage.getItem("participante") || []);
+    console.log(participantestorage.nombre || "No se cargo el nombre")
+
+    //Prueba de spread
+console.log(...imagenes)
 })
 
 
@@ -74,7 +87,7 @@ const btnEsPish = () => {
 
         if ((contador == 1) || (contador == 2) || (contador == 3)) {
             juego1.puntaje = juego1.puntaje + 1;
-            //Agrego toastify
+            //Agrego toastify si la respuesta es correcta
             Toastify({
                 text: "Respuesta correcta!",
                 duration: 3000,
@@ -93,35 +106,35 @@ const btnEsPish = () => {
         contador++;
     }
     else {
-
         contador++;
-        
-                //Agrego toastify
-                Toastify({
-                    text: "Respuesta incorrecta!",
-                    duration: 3000,
-                    newWindow: true,
-                    close: true,
-                    gravity: "bottom",
-                    position: "right",
-                    stopOnFocus: true,
-                    style: {
-                        background: "red",
-                    },
-                }).showToast();
+        //Agrego toastify si la respuesta es incorrecta
+        Toastify({
+            text: "Respuesta incorrecta!",
+            duration: 3000,
+            newWindow: true,
+            close: true,
+            gravity: "bottom",
+            position: "right",
+            stopOnFocus: true,
+            style: {
+                background: "red",
+            },
+        }).showToast();
         if (contador === 5) {
             let puntajeTotal = 25 * juego1.puntaje;
-            document.getElementById("participante").innerHTML = "Participante: " + Participante1.nombre + ", edad: " + Participante1.edad + " años, género: " + Participante1.genero + "<br>" + " Puntos obtenidos: " + puntajeTotal + "% <br> Respuestas correctas: " + juego1.puntaje;
-            document.getElementById('divBotones').style.display = "none";
-            document.getElementById('divBotones').classList.remove('d-block');
-            document.getElementById('divBotones').classList.add('d-none');
-            document.getElementById('divImg').style.display = "none";
-            document.getElementById('enunciado').style.display = "none";
-            document.getElementById('btnMostrarParticipante').style.display = "none";
+            //me fijo si se ingreso un nombre para mostrar
+            let participantestorage = JSON.parse(localStorage.getItem("participante"));
+
+            if (participantestorage.nombre) {
+                document.getElementById("participante").innerHTML = "Participante: " + Participante1.nombre + ", edad: " + Participante1.edad + " años, género: " + Participante1.genero + "<br>" + " Puntos obtenidos: " + puntajeTotal + "% <br> Respuestas correctas: " + juego1.puntaje;
+            } else {
+                document.getElementById("participante").innerHTML = "Participante anónimo " + "<br>" + " Puntos obtenidos: " + puntajeTotal + "% <br> Respuestas correctas: " + juego1.puntaje;
+            }
+            ocultarDivs();
 
         }
 
-        
+
     }
 }
 
@@ -136,51 +149,55 @@ const btnNoEsPish = () => {
             document.getElementById("participante").innerHTML = "Participante: " + Participante1.nombre + "<br>" + " Respuestas correctas: " + juego1.puntaje;
         }
         contador++;
-                //Agrego toastify
-                Toastify({
-                    text: "Respuesta incorrecta!",
-                    duration: 3000,
-                    newWindow: true,
-                    close: true,
-                    gravity: "bottom",
-                    position: "right",
-                    stopOnFocus: true,
-                    style: {
-                        background: "red",
-                    },
-                }).showToast();
+        //Agrego toastify si la respuesta es incorrecta
+        Toastify({
+            text: "Respuesta incorrecta!",
+            duration: 3000,
+            newWindow: true,
+            close: true,
+            gravity: "bottom",
+            position: "right",
+            stopOnFocus: true,
+            style: {
+                background: "red",
+            },
+        }).showToast();
     }
     else {
         contador++;
         if (contador === 5) {
             juego1.puntaje = juego1.puntaje + 1;
-                        //Agrego toastify
-                        Toastify({
-                            text: "Respuesta correcta!",
-                            duration: 3000,
-                            newWindow: true,
-                            close: true,
-                            gravity: "bottom",
-                            position: "right",
-                            stopOnFocus: true,
-                            style: {
-                                background: "green",
-                            },
-                        }).showToast();
+            //Agrego toastify si la respuesta es correcta
+            Toastify({
+                text: "Respuesta correcta!",
+                duration: 3000,
+                newWindow: true,
+                close: true,
+                gravity: "bottom",
+                position: "right",
+                stopOnFocus: true,
+                style: {
+                    background: "green",
+                },
+            }).showToast();
 
             let puntajeTotal = 25 * juego1.puntaje;
-            document.getElementById("participante").innerHTML = "Participante: " + Participante1.nombre + ", edad: " + Participante1.edad + " años, género: " + Participante1.genero + "<br>" + " Puntos obtenidos: " + puntajeTotal + "% <br> Respuestas correctas: " + juego1.puntaje;
-            document.getElementById('divBotones').style.display = "none";
-            document.getElementById('divBotones').classList.remove('d-block');
-            document.getElementById('divBotones').classList.add('d-none');
-            document.getElementById('divImg').style.display = "none";
-            document.getElementById('enunciado').style.display = "none";
-            document.getElementById('btnMostrarParticipante').style.display = "none";
+
+            //me fijo si se ingreso un nombre para mostrar
+            let participantestorage = JSON.parse(localStorage.getItem("participante"));
+
+            if (participantestorage.nombre) {
+                document.getElementById("participante").innerHTML = "Participante: " + Participante1.nombre + ", edad: " + Participante1.edad + " años, género: " + Participante1.genero + "<br>" + " Puntos obtenidos: " + puntajeTotal + "% <br> Respuestas correctas: " + juego1.puntaje;
+            } else {
+                document.getElementById("participante").innerHTML = "Participante anónimo " + "<br>" + " Puntos obtenidos: " + puntajeTotal + "% <br> Respuestas correctas: " + juego1.puntaje;
+            }
+
+            ocultarDivs();
         }
     }
 }
 
-//Resgistro algunos eventos
+//Resgistro algunos eventos y los mustro por consola
 const nombre = document.getElementById("nombre");
 const edad = document.getElementById("edad");
 const genero = document.getElementById("genero");
@@ -196,8 +213,19 @@ genero.addEventListener("change", () => {
 })
 
 
-//Recupero participante del localStorage
+//Recupero participante del localStorage con un botón
 const btnMostrarParticipante = () => {
     let participantestorage = JSON.parse(localStorage.getItem("participante"));
-    console.log(participantestorage);
+
+    if (participantestorage.nombre) {
+        console.log(participantestorage);
+    }
+}
+//Función para mostrar/ocultar el texto en la actvidad
+function ocultarDivs() {
+    document.getElementById('divBotones').style.display = "none";
+    document.getElementById('divBotones').classList.remove('d-block');
+    document.getElementById('divBotones').classList.add('d-none');
+    document.getElementById('divImg').style.display = "none";
+    document.getElementById('enunciado').style.display = "none";
 }
